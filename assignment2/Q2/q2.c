4e57f4ebbad05c4a8bd35864445c3efe26fa1d24
Emad Kufiya <engr.mody@hotmail.com>
2016-08-06 11:26:15 +0300

    Assignment_2

---
+++ assignment2/Q2/q2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+printf("\nGregorian Months :\n\n1-January\t2-February\t3-March\t\t");
+printf("4-April\n5-May\t\t6-June\t\t7-July\t\t8-August\n9-September");
+printf("\t10-October\t11-November\t12-December\n\n");
+return 0; 
+}
